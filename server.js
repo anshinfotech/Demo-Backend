0ca7da5fdd9a679c5957fcf73668995737f471ab
paramlotey <plotey88@gmail.com>
2024-02-08 09:45:00 +0530

    f

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
   {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin:"https://demo-frontend-mlfr.onrender.com",
     credentials: true
   }
 ));
